3a6e621cf2bb27fc992449469019496bb99cc025
Tynka <martyna05p@gmail.com>
2025-01-06 21:10:00 +0100

    z5

--- zestaw5.R
+++ zestaw5.R
@@ -33,8 +33,53 @@ quantile(dane, probs=c(2/3, 0.072, 0.974))
 #6.748 6.950 7.274 
 
 #ZADANIE 2
+#DUŻE!!!
+n=length(dane)
+round(sqrt(n)) 
+k=10
+a=0.01 #to alfa
+mi=min(dane)
+ma=max(dane)
+R=ma-mi
+#dlugość przedziału
+b=ceiling((R+a/2)/(k*a))*a
+b
+mi-a/2+k*b #oh, nie wiem co to
+#wektor krańca przedziałów
+kr=mi-a/2+b*c(0:k)
+kr
+
+#HISTOGRAM
+h=hist(dane, breaks=kr, plot=F)
+#SZEREG ROZDZIELCZY
+przedział=1:k
+lewy_kr=kr[1:k]
+prawy_kr=kr[2:(k+1)]
+lewy_kr
+prawy_kr
+
+#liczebność
+liczność_klasy=h$counts
+
+skumulowana=cumsum(h$counts)/n
+
+wspolrzedna_x <- (head(kr, -1) + tail(kr, -1)) / 2
+cumulative_freq_table <- data.frame(wspolrzedna_x, skumulowana)
+cumulative_freq_table
+
+wielobok=plot(cumulative_freq_table$wspolrzedna_x, cumulative_freq_table$skumulowana, 
+     type = "l", col = "red4", xlab = "Wynagrodzenie (tys. zł)", ylab = "Częstość skumulowana",
+     main = "Wielobok częstości skumulowanych")
+abline(h = c(0.25, 0.5, 0.75), col = c("orange", "coral2", "coral"), lty = 4)  # Linie dla Q1, Q2, Q3
+czestoscwartosci=round(liczność_klasy/n,3)
+
+Szereg_rozdzielczy=as.data.frame(cbind(przedział,lewy_kr,prawy_kr,liczebność, czestoscwartosci))
+Szereg_rozdzielczy
 
 
+
+
+write.csv(Szereg_rozdzielczy_duze, "szereg_rozdzielczy.csv", row.names = FALSE, fileEncoding = "UTF-8")
 
 
 
