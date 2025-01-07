0269cd518affff02d77682665473207d6de9f450
Tynka <martyna05p@gmail.com>
2025-01-07 16:44:13 +0100

    z6

--- zestaw6.R
+++ zestaw6.R
@@ -12,11 +12,57 @@ mean(dane$wzrost)
 #172.2
 
 #ODCHYLENIE STANDARDOWE POLICZYC RECZNIE BO R COS INACZEJ
-sd(dane$wzrost)
-#16.674
+odchyl_wzrost=sqrt(1/n*sum((wzrost-śr_wzrost)^2))
+#16.1088
 
 #a WAGA
 mean(dane$waga)
 #71.8
-sd(dane$waga)
-#15.043
+odchyl_waga=sqrt(1/n*sum((waga-śr_waga)^2))
+#14.53364
+
+
+#b KOWARIANCJA 
+n=length(waga)
+śr_waga=mean(dane$waga)
+odchyl_waga=sqrt(1/n*sum((dane$waga-śr_waga)^2))
+śr_wzrost=mean(dane$wzrost)
+odchyl_wzrost=sqrt(1/n*sum((dane$wzrost-śr_wzrost)^2))
+
+#kowariancja
+kow=1/n*sum((wzrost-śr_wzrost)*(waga-śr_waga))
+kow
+#230.9733
+#ewentualnie
+cov(Wzrost,Waga) #wyznaczana z mianownikiem n-1
+1/(n-1)*sum((Wzrost-śr_wzrost)*(Waga-śr_waga))
+
+
+#wsp korelacji
+kor=kow/(odchyl_wzrost*odchyl_waga)
+kor
+#ewentualnie
+cor(wzrost,waga)
+#0.986562
+
+#ZADANIE C
+
+#wyznaczymy prostą regresji
+
+#wykres korelacyjny wagi od wzrostu (raczej waga od wzrostu zależy)
+
+plot(wzrost,waga,lwd=3)
+
+#wyznaczmy współczynniki w regresji linowej
+A=kor*odchyl_waga/odchyl_wzrost
+B=śr_waga-A*śr_wzrost
+A
+B
+
+
+
+#nanieśmy linię regresji na wykres korelacyjny
+#wykorzytamy funkcję points() - połączymy odpowiednie punkty
+plot(wzrost,waga,lwd=3)
+points(c(min(wzrost),max(wzrost)),
+       c(A*min(wzrost)+B,A*max(wzrost)+B),type="l",col="blue")
